9b10fdaf66df1945d078e65f10e5d8cf4ae9dbf9
Benjamin Fregoso <benackffbenacker@hotmail.com>
2016-10-04 12:27:51 -0500

    Programa terminado

--- Tarea06.py
+++ Tarea06.py
@@ -1,2 +1,7 @@
 #Inicio de programa
 print("CALCULAR EL AREA DE UN CIRCULO")
+radio = input("Introduzca el radio en cm: ")
+radio_int = int(radio)
+areac = (radio_int*radio_int)*3.14159265
+print("El area del circulo es: ", areac , " cm2" )
+exit()
